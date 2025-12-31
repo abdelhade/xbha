f3f05a93cfd82a208f30d5301284eee64b9030fb
unknown <mariamaboshalby>
2025-12-31 16:44:26 +0200

    edit errors

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             TenantSeeder::class,
             CategorySeeder::class,
             AdminSeeder::class,
+            
           
         ]);
     }
